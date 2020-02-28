927ecd8866706729300dd1773a76e24744471977
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-28 19:09:22 +0530

    Delete Solution.sql

--- Alternative Queries/Draw The Triangle 2/Solution.sql
+++
@@ -1,6 +0,0 @@
-/*
-Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-*/
-SELECT RPAD('*', 2 * LEVEL - 1, ' *')
-FROM DUAL CONNECT BY LEVEL <= 20;
