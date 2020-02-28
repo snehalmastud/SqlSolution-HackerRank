28e7bb1f2023dc4979be7e7c075a2004b2e15fc1
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-28 17:54:24 +0530

    Basic Select!

---
+++ Basic Select/Weather Observation Station 6/Solution.sql
@@ -0,0 +1,7 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE UPPER(SUBSTR(CITY, 1, 1)) IN ('A','E','I','O','U');
